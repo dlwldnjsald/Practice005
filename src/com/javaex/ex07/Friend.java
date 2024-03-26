299df81720183141b906417da5f1d60f0d6c3b16
806-26 <806-26@DESKTOP-8V02G4F>
2024-03-26 16:05:13 +0900

    c

---
+++ src/com/javaex/ex07/Friend.java
@@ -0,0 +1,82 @@
+
+package com.javaex.ex07;  //
+
+
+
+public class Friend {
+
+
+
+    private String name;
+
+    private String hp;
+
+    private String school;
+
+
+
+    
+
+    // getter/setter 작성
+
+    public String getName() {
+
+    	return name;
+
+    }
+
+    
+
+    public void setName(String name) {		//외부로부터 name변수에 받아들인 데이터를 하위 블록에 집어넣을것. 
+
+    	this.name = name;      //name변수에 할당된 외부데이터를 다시 this.name에 할당해준다
+
+    }
+
+    
+
+   //------------------------------------
+
+    public String getHp() {
+
+    	return hp;
+
+    }
+
+   
+
+    public void setHp(String hp) {
+
+    	this.hp = hp;
+
+    }
+
+    //----------------------------------
+
+    public String getSchool() {
+
+    	return school;
+
+    }
+
+    
+
+    public void setSchool(String school) {
+
+    	this.school = school;
+
+    }
+
+    //----------------------------------------
+
+    
+
+    public void showInfo(){
+
+        System.out.println("이름:" + name + "  핸드폰:" + hp + " 학교:" + school);
+
+    }
+
+
+
+}
---
+++ src/com/javaex/ex07/FriendApp.java
@@ -0,0 +1,101 @@
+ package com.javaex.ex07; //
+
+
+
+import java.util.Scanner;
+
+
+
+public class FriendApp {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        Friend[] friendArray = new Friend[3];    //fried배열 3개 만들어주기 (length==3인)배열
+
+        Scanner sc = new Scanner(System.in);	//스캐너 열기 개방
+
+
+
+        System.out.println("친구를 3명 등록해 주세요");	//프롬프트를 출력했음
+
+
+
+        
+
+        //for문 시작 루프돌리기 //
+
+        //친구정보 3명 입력 로직 --> 반복문 사용
+
+        for (int i = 0; i < 3; i++) {  //for(초기화,조건연산,증감)
+
+        	
+
+        	//이제 데이터 입력해주기
+
+         	String name,hp,school;			//변수 선언하기
+
+         	
+
+         	System.out.print("이름: ");
+
+         	name = sc.next();	//스캐너로부터 문자열을 전달받는다
+
+        	System.out.print("핸드폰: ");
+
+         	hp = sc.next();		//스캐너로부터 문자열을 전달받는다
+
+        	System.out.print("학교: ");
+
+         	school = sc.next();	//스캐너로부터 문자열을 전달받는다
+
+        	
+
+        	Friend newFriend = new Friend();   //주의) 메모리상에 friend를 만들어주고
+
+        	newFriend.setName(name);
+
+        	newFriend.setHp(hp);
+
+        	newFriend.setSchool(school);   //만들어 두고 이제 연결시키기
+
+        	
+
+        	//배열에 참조주소 할당해주기//
+
+        	friendArray[i] = newFriend;
+
+        	System.out.println("------------------------------");
+
+        		
+
+        }  
+
+        //for문 끝
+
+        
+
+        
+
+        // 친구정보 출력
+
+        for (int i = 0; i < friendArray.length; i++) {
+
+            //친구정보 출력 메소드 호출
+
+        	friendArray[i].showInfo();
+
+        }
+
+
+
+        sc.close();
+
+    }
+
+
+
+}
